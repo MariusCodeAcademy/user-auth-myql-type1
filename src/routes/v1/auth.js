04fad56305950361418122908b0ccdedac8a09ce
Marius Kuzminskas <marius.kuzminskas@ca.com>
2021-10-11 11:50:52 +0300

    login stuff

--- src/routes/v1/auth.js
+++ src/routes/v1/auth.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const bcrypt = require('bcryptjs');
-const joi = require('joi');
+// const joi = require('joi');
 
 const router = express.Router();
 
@@ -43,5 +43,34 @@ router.post('/register', async (req, res) => {
 });
 
 // POST /auth/login - checks if user exists and if passswords match
+router.post('/login', async (req, res) => {
+  const { body } = req;
+
+  // validate body using joi
+  const validateResult = await validateRegister(body, res);
+  if (validateResult === false) return;
+
+  try {
+    const conn = await mysql.createConnection(dbConfig);
+    // check if we have a user email in our table
+    const sql = 'SELECT * FROM users WHERE email = ?';
+    const [foundUser] = await conn.execute(sql, [body.email]);
+    await conn.end();
+    // console.log(foundUser);
+    if (foundUser.length === 0) {
+      throw new Error('user not found');
+    }
+    // if so, check if passwors match
+    if (bcrypt.compareSync(body.password, foundUser[0].password)) {
+      // eslint-disable-next-line consistent-return
+      return res.send({ msg: 'user found', foundUser: foundUser[0].email });
+    }
+    throw new Error('password do not match');
+  } catch (error) {
+    // if no user found
+    console.log('errror loging in', error.message);
+    res.status(500).send({ error: error.message });
+  }
+});
 
 module.exports = router;
