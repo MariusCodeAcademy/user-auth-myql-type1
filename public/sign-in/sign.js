98f040e220ed8eb6ad69f3d5c7bb76433260f6c3
Marius Kuzminskas <marius.kuzminskas@ca.com>
2021-10-12 11:09:48 +0300

    posts backend uzd

--- public/sign-in/sign.js
+++ public/sign-in/sign.js
@@ -1,6 +1,7 @@
 //
 const URL = 'http://localhost:3000/auth';
 const form = document.querySelector('.form-signin');
+const navbar = document.querySelector('.navbar-nav');
 
 form.addEventListener('submit', async (e) => {
   e.preventDefault();
@@ -17,8 +18,27 @@ form.addEventListener('submit', async (e) => {
   if (respInJs.msg === 'user found') {
     localStorage.setItem('loggedInUserToken', respInJs.token);
     localStorage.setItem('loggedInUserEmail', respInJs.foundUser);
+    showUserInNavbar();
+    showPostsInNavbar();
   }
   console.log('got back', respInJs);
 });
 
-console.log(localStorage.getItem('loggedInUserEmail'));
+// console.log(localStorage.getItem('loggedInUserEmail'));
+
+function loggedIn() {
+  if (localStorage.getItem('loggedInUserEmail')) {
+    showUserInNavbar();
+    showPostsInNavbar();
+  }
+}
+loggedIn();
+
+function showUserInNavbar() {
+  navbar.innerHTML += `<a class="nav-link" disabled href="#">User: ${localStorage.getItem(
+    'loggedInUserEmail',
+  )}</a>`;
+}
+function showPostsInNavbar() {
+  navbar.innerHTML += '<a class="nav-link" href="/public/">Posts</a>';
+}
---
+++ src/routes/v1/posts.js
@@ -0,0 +1,3 @@
+// GET /posts - get all posts return posts title, timestamp and user email
+
+// GET /posts - with authorization to get posts of current user
